ccf2bb78eded342fc82667cafcda265a9353334f
spadaritucks <thiago.spadari02@gmail.com>
2025-03-04 14:52:51 -0300

    commit

--- src/pages/cadastro.tsx
+++ src/pages/cadastro.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-expressions */
+
 import { useState } from "react";
 import cepService from "../services/cep.service";
 import Input from "../components/input";
